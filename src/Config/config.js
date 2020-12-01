2e2f41b9609af9f0b099600971c4a9ef86af011d
bettercallazamat <bettercallazamat@gmail.com>
2020-12-02 03:51:43 +0600

    Apply usage of template

---
+++ src/Config/config.js
@@ -0,0 +1,9 @@
+// import 'phaser';
+import Phaser from 'phaser';
+
+export default {
+  type: Phaser.AUTO,
+  parent: 'phaser-example',
+  width: 800,
+  height: 600
+};
---
+++ src/Scenes/GameScene.js
@@ -0,0 +1,18 @@
+// import 'phaser';
+import Phaser from 'phaser';
+// import logo from '../assets/logo'
+
+export default class GameScene extends Phaser.Scene {
+  constructor () {
+    super('Game');
+  }
+
+  preload () {
+    // load images
+    this.load.image('logo', 'src/assets/logo.png');
+  }
+
+  create () {
+    this.add.image(400, 300, 'logo');
+  }
+};
